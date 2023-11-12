d1c83b4df9d2b0abf6af9d2983ad2b0824a8f02c
zé <128116145+JjJjJose@users.noreply.github.com>
2023-11-12 01:21:05 -0300

    Update index.php

--- pedidos/index.php
+++ pedidos/index.php
@@ -95,7 +95,7 @@
                         </select>
                     </div>
 
-                    <div class="form-floating mb-2 myRow">
+                    <div class="form-floating mb-2 myRow'">
                         <p>Áreas de competência:</p>
                         <div id="dummy">
                             <select class="selectpicker" multiple data-live-search="true" id="subareaselect">
@@ -215,5 +215,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="./pedidos.js"></script>
 </body>
-
 </html>
+
+<?php ?>
