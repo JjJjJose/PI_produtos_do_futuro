424b98867cb05049bce90d347e1d27f08bc28e0d
zé <128116145+JjJjJose@users.noreply.github.com>
2023-11-17 14:08:23 -0300

    shopping

---
+++ shopping/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <div class="col-md-3 mb-2 mb-md-0">
+            <a href="#" class="d-inline-flex link-body-emphasis text-decoration-none">
+                <svg class="bi" width="40" height="32" role="img" aria-label="logo">
+                    <img src="../assets/logo.png" style="width: 40px;" alt="" srcset="">
+                </svg>
+            </a>
+        </div>
+
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="#" class="nav-link px-2">Inicio</a></li>
+            <li><a href="#" class="nav-link px-2">Serviços</a></li>
+            <li><a href="#" class="nav-link px-2">FAQS</a></li>
+            <li><a href="#" class="nav-link px-2">Sobre</a></li>
+        </ul>
+
+        <div class="col-md-3 text-end">
+            <a href=""><button type="button" class="btn btn-outline-primary me-2">Login</button></a>
+            <a href=""><button type="button" class="btn btn-primary">Cadastro</button></a>
+        </div>
+    </header>
+
+
+
+
+
+
+
+
+    
+
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <div class="col-md-4 d-flex align-items-center">
+            <a href="" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                <svg class="bi" width="30" height="24">
+                    <img src="../assets/logo.png" style="width: 40px;">
+                </svg>
+            </a>
+            <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Produtos do Futuro</span>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
